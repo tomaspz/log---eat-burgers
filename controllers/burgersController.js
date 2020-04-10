a664f2259ce339c03646898f1c9167addde51bd4
Tomas Perez <tomaspz@gmail.com>
2020-04-09 22:39:00 -0400

    add screenshot and work on page css

--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -8,17 +8,19 @@ var burger = require("../models/burger.js");
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
   burger.selectAll(function(data) {
-    var hbsObject = {
+    var handlebarsObject = {
       burgers: data
     };
-    console.log(hbsObject);
-    res.render("index", hbsObject);
+    res.render("index", handlebarsObject);
   });
 });
 
 router.post("/api/burgers", function(req, res) {
-  burger.insertOne(["burger_name", "devoured"], [req.body.burger_name, req.body.devoured], function(result) {
-    // Send back the ID of the new quote
+  burger.insertOne(
+    ["burger_name"], 
+    [req.body.burger_name], 
+    function(result) {
+    // Send back the ID of the new burger
     res.json({ id: result.insertId });
   });
 });
@@ -26,20 +28,17 @@ router.post("/api/burgers", function(req, res) {
 router.put("/api/burgers/:id", function(req, res) {
   var condition = "id = " + req.params.id;
 
-  console.log("condition", condition);
-
   burger.updateOne(
     {
-      devoured: req.body.devoured
+      devoured: true
     },
     condition,
     function(result) {
-      if (result.changedRows === 0) {
-        // If no rows were changed, then the ID must not exist, so 404
-        return res.status(404).end();
-      }
+      // if (result.changedRows === 0) {
+      //   // If no rows were changed, then the ID must not exist, so 404
+      //   return res.status(404).end();
+      // }
       res.status(200).end();
-
     }
   );
 });
